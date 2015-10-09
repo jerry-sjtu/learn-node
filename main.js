88080fb6c9a573dcc3f537d356d00f87a031ca49
wangqiang <wangqiang@ymatou.com>
2015-10-09 03:19:57 +0100

    more example is added.

---
+++ main.js
@@ -0,0 +1,14 @@
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('example')
+);
+
+// var unique = require('uniq');
+
+// var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
+
+// console.log(unique(data));
